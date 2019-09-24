02e87d4e7090ad4f7594fe4a9e3009c4f5ec71a2
AvalZ <avalenza89@gmail.com>
2019-09-24 16:56:31 +0200

    Update master doc

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ napoleon_google_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
