2f73fb4da28a5a08fd88459aecfa51bab0991f2c
Biagio Montaruli <biagio.montaruli@eurecom.fr>
2024-02-15 17:09:05 +0000

    Further improve ModSec wrapper

--- wafamole/models/modsec_wrapper.py
+++ wafamole/models/modsec_wrapper.py
@@ -10,28 +10,29 @@ from wafamole.models import Model
 
 import os
 from pathlib import Path
+import re
 from urllib.parse import urlparse, urlencode
 from enum import Enum
 
 
 class Severity(Enum):
-  def __new__(cls, *args, **kwds):
-    value = len(cls.__members__)
-    obj = object.__new__(cls)
-    obj._value_ = value
-    return obj
-  def __init__(self, severity_id, score):
-    self.id = severity_id
-    self.score = score
+    def __new__(cls, *args, **kwds):
+        value = len(cls.__members__)
+        obj = object.__new__(cls)
+        obj._value_ = value
+        return obj
+    def __init__(self, severity_id, score):
+        self.id = severity_id
+        self.score = score
  
-  EMERGENCY = 0, 0 # not used in CRS
-  ALERT     = 1, 0 # not used in CRS
-  CRITICAL  = 2, 5
-  ERROR     = 3, 4
-  WARNING   = 4, 3
-  NOTICE    = 5, 2
-  INFO      = 6, 0 # not used in CRS
-  DEBUG     = 7, 0 # not used in CRS
+    EMERGENCY = 0, 0 # not used in CRS
+    ALERT     = 1, 0 # not used in CRS
+    CRITICAL  = 2, 5
+    ERROR     = 3, 4
+    WARNING   = 4, 3
+    NOTICE    = 5, 2
+    INFO      = 6, 0 # not used in CRS
+    DEBUG     = 7, 0 # not used in CRS
 
 
 class PyModSecurityWrapper(Model):
@@ -43,6 +44,15 @@ class PyModSecurityWrapper(Model):
         self.rules_path = Path(rules_path)
         self.modsec = ModSecurity()
         self.paranoia_level = pl
+
+        # Ensure the right PL level is set in the CRS config file.
+        # Here we assume that the PL is explicitely set using the 900000 rule.
+        with open(self.rules_path / 'crs-setup.conf', 'r+') as crs_config_file:
+            file_content = crs_config_file.read()
+            file_content = re.sub(r"setvar:tx.blocking_paranoia_level=\d", "setvar:tx.blocking_paranoia_level={}".format(pl), file_content)
+            crs_config_file.seek(0)
+            crs_config_file.write(file_content)
+            crs_config_file.truncate()
 
         self.rules = RulesSet()
 
@@ -78,7 +88,7 @@ class PyModSecurityWrapper(Model):
         transaction = Transaction(self.modsec, self.rules)
 
         transaction.processURI(full_url, method, "2.0")
-        
+
         # Headers
         headers = {
             "Host": parsed_url.netloc, # Avoid matching rule 920280
